fa3ebccffa273b9611ed85e9195ce508135236a4
chocketa <jonatanlakes1998@gmail.com>
2025-10-10 15:03:21 +0300

    feat: util

---
+++ src/main/java/me/choketa/crazyeggs/utils/Pair.java
@@ -0,0 +1,16 @@
+package me.choketa.crazyeggs.utils;
+
+public class Pair<A, B>{
+    private final A a;
+    private final B b;
+    public Pair(A a, B b) {
+        this.a=a;
+        this.b=b;
+    }
+    public A a(){
+        return a;
+    }
+    public B b(){
+        return b;
+    }
+}
